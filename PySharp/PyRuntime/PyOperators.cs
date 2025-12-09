d5d8f6baaf7b271e47f8ba9fc505a75535a32b80
Silencersn <silencersn@outlook.com>
2025-12-09 22:36:37 +0800

    重构 PyOperators 操作符逻辑，优化性能
    重构了 `PyOperators` 类中的操作符处理逻辑，引入了 `FastOperatorForInt` 方法以优化整数操作符的性能，支持多种运算类型。新增 `LeftReflectiveOperator` 和 `RightReflectiveOperator` 方法，替代原先的 `Func<PyObject?>` 参数传递方式。修改 `ReflectiveOperator` 方法，整合整数快速处理和反射逻辑，支持可选 `modulo` 参数。
    
    简化了所有操作符方法（如 `Add`、`Sub` 等），直接调用新的 `ReflectiveOperator` 方法。为整数运算添加了除零错误和幂运算模逻辑的处理。删除冗余代码，提升代码可读性和维护性，同时修复了部分错误提示信息中的细节问题。

--- PySharp/PyRuntime/PyOperators.cs
+++ PySharp/PyRuntime/PyOperators.cs
@@ -1,82 +1,415 @@
 ﻿using PySharp.PyModules.Builtins;
 using System.Diagnostics;
+using System.Numerics;
+using System.Reflection;
 
 namespace PySharp.PyRuntime;
 
 public static class PyOperators
 {
-    private static PyObject? ReflectiveOperator(string operatorDisplay, PyObject left, PyObject right, Func<PyObject?> leftOp, Func<PyObject?> rightOp)
+    private static PyObject? FastOperatorForInt(string op, PyIntObject left, PyIntObject right, PyIntObject? modulo)
     {
-        if (left.PyType.IsSubclass(right.PyType) && left.PyType != right.PyType)
-            (leftOp, rightOp) = (rightOp, leftOp);
+        switch (op)
+        {
+            case "+":
+                return PyIntObject.FromInteger(left.Value + right.Value);
 
-        var ret = leftOp();
-        if (ret is not PyNotImplementedObject) // null or any value other than NotImplemented
-            return ret;
+            case "-":
+                return PyIntObject.FromInteger(left.Value - right.Value);
 
-        ret = rightOp();
+            case "*":
+                return PyIntObject.FromInteger(left.Value * right.Value);
+
+            case "/":
+                if (right.Value == 0)
+                    return PyVirtualMachine.RaiseZeroDivisionError("division by zero");
+                return PyFloatObject.FromDouble((double)left.Value / (double)right.Value);
+
+            case "//":
+                if (right.Value == 0)
+                    return PyVirtualMachine.RaiseZeroDivisionError("division by zero");
+                var (q, r) = BigInteger.DivRem(left.Value, right.Value);
+                if (r.IsZero || BigInteger.IsPositive(q))
+                    return PyIntObject.FromInteger(q);
+                return PyIntObject.FromInteger(q - 1);
+
+            case "%":
+                if (right.Value == 0)
+                    return PyVirtualMachine.RaiseZeroDivisionError("integer modulo by zero");
+                return PyIntObject.FromInteger(left.Value % right.Value);
+
+            case "**":
+                Debug.Assert(modulo is not null);
+                if (modulo.Value == 0)
+                {
+                    if (right.Value >= 0)
+                        return PyIntObject.FromInteger(BigInteger.Pow(left.Value, (int)right.Value));
+                    return PyFloatObject.FromDouble(System.Math.Pow((double)left.Value, (double)right.Value));
+                }
+                else
+                {
+                    if (right.Value >= 0)
+                        return PyIntObject.FromInteger(BigInteger.ModPow(left.Value, right.Value, modulo.Value));
+                    return PyFloatObject.FromDouble(System.Math.Pow((double)left.Value, (double)right.Value) % (double)modulo.Value);
+                }
+
+            case "<<":
+                return PyIntObject.FromInteger(left.Value << right.Int32Value);
+
+            case ">>":
+                return PyIntObject.FromInteger(left.Value >> right.Int32Value);
+
+            case "&":
+                return PyIntObject.FromInteger(left.Value & right.Value);
+
+            case "|":
+                return PyIntObject.FromInteger(left.Value | right.Value);
+
+            case "^":
+                return PyIntObject.FromInteger(left.Value ^ right.Value);
+
+            case "<":
+                return PyBoolObject.FromBoolean(left.Value < right.Value);
+
+            case "<=":
+                return PyBoolObject.FromBoolean(left.Value <= right.Value);
+
+            case "==":
+                return PyBoolObject.FromBoolean(left.Value == right.Value);
+
+            case "!=" :
+                return PyBoolObject.FromBoolean(left.Value != right.Value);
+
+            case ">":
+                return PyBoolObject.FromBoolean(left.Value > right.Value);
+
+            case ">=":
+                return PyBoolObject.FromBoolean(left.Value >= right.Value);
+
+            default:
+                throw new UnreachableException();
+        }
+    }
+
+    private static PyObject? LeftReflectiveOperator(string op, PyObject left, PyObject right, PyObject? modulo)
+    {
+        PyObject? ret;
+        switch (op)
+        {
+            case "+":
+                ret = left.Add(right);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = right.RAdd(left);
+                break;
+
+            case "-":
+                ret = left.Sub(right);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = right.RSub(left);
+                break;
+
+            case "*":
+                ret = left.Mul(right);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = right.RMul(left);
+                break;
+
+            case "/":
+                ret = left.TrueDiv(right);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = right.RTrueDiv(left);
+                break;
+
+            case "//":
+                ret = left.FloorDiv(right);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = right.RFloorDiv(left);
+                break;
+
+            case "%":
+                ret = left.Mod(right);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = right.RMod(left);
+                break;
+
+            case "**":
+                Debug.Assert(modulo is not null);
+                ret = left.Pow(right, modulo);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = right.RPow(left, modulo);
+                break;
+
+            case "<<":
+                ret = left.LShift(right);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = right.RLShift(left);
+                break;
+
+            case ">>":
+                ret = left.RShift(right);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = right.RRShift(left);
+                break;
+
+            case "&":
+                ret = left.And(right);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = right.RAnd(left);
+                break;
+
+            case "^":
+                ret = left.Xor(right);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = right.RXor(left);
+                break;
+
+            case "|":
+                ret = left.Or(right);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = right.ROr(left);
+                break;
+
+            case "<":
+                ret = left.Lt(right);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = right.Gt(left);
+                break;
+
+            case "<=":
+                ret = left.Le(right);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = right.Ge(left);
+                break;
+
+            case ">":
+                ret = left.Gt(right);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = right.Lt(left);
+                break;
+
+            case ">=":
+                ret = left.Ge(right);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = right.Le(left);
+                break;
+
+            default:
+                return PyVirtualMachine.RaiseTypeError($"Operator '{op}' is not supported.");
+        }
+
         if (ret is PyNotImplementedObject)
-            return PyVirtualMachine.RaiseTypeError($"'{operatorDisplay}' not supported between instances of '{left.PyType.Name}' and '{right.PyType.Name}'");
+            return PyVirtualMachine.RaiseTypeError($"'{op}' not supported between instances of '{left.PyType.Name}' and '{right.PyType.Name}'");
 
         return ret;
+    }
+    private static PyObject? RightReflectiveOperator(string op, PyObject left, PyObject right, PyObject? modulo)
+    {
+        PyObject? ret;
+        switch (op)
+        {
+            case "+":
+                ret = right.RAdd(left);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = left.Add(right);
+                break;
+
+            case "-":
+                ret = right.RSub(left);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = left.Sub(right);
+                break;
+
+            case "*":
+                ret = right.RMul(left);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = left.Mul(right);
+                break;
+
+            case "/":
+                ret = right.RTrueDiv(left);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = left.TrueDiv(right);
+                break;
+
+            case "//":
+                ret = right.RFloorDiv(left);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = left.FloorDiv(right);
+                break;
+
+            case "%":
+                ret = right.RMod(left);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = left.Mod(right);
+                break;
+
+            case "**":
+                Debug.Assert(modulo is not null);
+                ret = right.RPow(left, modulo);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = left.Pow(right, modulo);
+                break;
+
+            case "<<":
+                ret = right.RLShift(left);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = left.LShift(right);
+                break;
+
+            case ">>":
+                ret = right.RRShift(left);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = left.RShift(right);
+                break;
+
+            case "&":
+                ret = right.RAnd(left);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = left.And(right);
+                break;
+
+            case "^":
+                ret = right.RXor(left);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = left.Xor(right);
+                break;
+
+            case "|":
+                ret = right.ROr(left);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = left.Or(right);
+                break;
+
+            case "<":
+                ret = right.Gt(left);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = left.Lt(right);
+                break;
+
+            case "<=":
+                ret = right.Ge(left);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = left.Le(right);
+                break;
+
+            case ">":
+                ret = right.Lt(left);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = left.Gt(right);
+                break;
+
+            case ">=":
+                ret = right.Le(left);
+                if (ret is not PyNotImplementedObject)
+                    return ret;
+                ret = left.Ge(right);
+                break;
+
+            default:
+                return PyVirtualMachine.RaiseTypeError($"Operator '{op}' is not supported.");
+        }
+
+        if (ret is PyNotImplementedObject)
+            return PyVirtualMachine.RaiseTypeError($"'{op}' not supported between instances of '{right.PyType.Name}' and '{left.PyType.Name}'");
+
+        return ret;
+    }
+    private static PyObject? ReflectiveOperator(string op, PyObject left, PyObject right, PyObject? modulo = null)
+    {
+        if (left is PyIntObject leftInt && right is PyIntObject rightInt && modulo is null or PyIntObject)
+            return FastOperatorForInt(op, leftInt, rightInt, modulo as PyIntObject);
+
+        if (left.PyType.IsSubclass(right.PyType) && left.PyType != right.PyType)
+            return RightReflectiveOperator(op, left, right, modulo);
+        return LeftReflectiveOperator(op, left, right, modulo);
     }
 
     public static PyObject? Add(PyObject left, PyObject right)
     {
-        return ReflectiveOperator("+", left, right, () => left.Add(right), () => right.RAdd(left));
+        return ReflectiveOperator("+", left, right);
     }
     public static PyObject? Sub(PyObject left, PyObject right)
     {
-        return ReflectiveOperator("-", left, right, () => left.Sub(right), () => right.RSub(left));
+        return ReflectiveOperator("-", left, right);
     }
     public static PyObject? Mul(PyObject left, PyObject right)
     {
-        return ReflectiveOperator("*", left, right, () => left.Mul(right), () => right.RMul(left));
+        return ReflectiveOperator("*", left, right);
     }
     public static PyObject? TrueDiv(PyObject left, PyObject right)
     {
-        return ReflectiveOperator("/", left, right, () => left.TrueDiv(right), () => right.RTrueDiv(left));
+        return ReflectiveOperator("/", left, right);
     }
     public static PyObject? FloorDiv(PyObject left, PyObject right)
     {
-        return ReflectiveOperator("//", left, right, () => left.FloorDiv(right), () => right.RFloorDiv(left));
+        return ReflectiveOperator("//", left, right);
     }
     public static PyObject? Mod(PyObject left, PyObject right)
     {
-        return ReflectiveOperator("%", left, right, () => left.Mod(right), () => right.RMod(left));
+        return ReflectiveOperator("%", left, right);
     }
     public static PyObject? Pow(PyObject left, PyObject right, PyObject modulo)
     {
-        return ReflectiveOperator("**", left, right, () => left.Pow(right, modulo), () => right.RPow(left, modulo));
+        return ReflectiveOperator("**", left, right, modulo);
     }
     public static PyObject? LShift(PyObject left, PyObject right)
     {
-        return ReflectiveOperator("<<", left, right, () => left.LShift(right), () => right.RLShift(left));
+        return ReflectiveOperator("<<", left, right);
     }
     public static PyObject? RShift(PyObject left, PyObject right)
     {
-        return ReflectiveOperator(">>", left, right, () => left.RShift(right), () => right.RRShift(left));
+        return ReflectiveOperator(">>", left, right);
     }
     public static PyObject? And(PyObject left, PyObject right)
     {
-        return ReflectiveOperator("&", left, right, () => left.And(right), () => right.RAnd(left));
+        return ReflectiveOperator("&", left, right);
     }
     public static PyObject? Xor(PyObject left, PyObject right)
     {
-        return ReflectiveOperator("^", left, right, () => left.Xor(right), () => right.RXor(left));
+        return ReflectiveOperator("^", left, right);
     }
     public static PyObject? Or(PyObject left, PyObject right)
     {
-        return ReflectiveOperator("|", left, right, () => left.Or(right), () => right.ROr(left));
+        return ReflectiveOperator("|", left, right);
     }
-
     public static PyObject? Lt(PyObject left, PyObject right)
     {
-        return ReflectiveOperator("<", left, right, () => left.Lt(right), () => right.Gt(left));
+        return ReflectiveOperator("<", left, right);
     }
     public static PyObject? Le(PyObject left, PyObject right)
     {
-        return ReflectiveOperator("<=", left, right, () => left.Le(right), () => right.Ge(left));
+        return ReflectiveOperator("<=", left, right);
     }
     public static PyObject? Eq(PyObject left, PyObject right)
     {
@@ -117,11 +450,11 @@ public static class PyOperators
     }
     public static PyObject? Gt(PyObject left, PyObject right)
     {
-        return ReflectiveOperator(">", left, right, () => left.Gt(right), () => right.Lt(left));
+        return ReflectiveOperator(">", left, right);
     }
     public static PyObject? Ge(PyObject left, PyObject right)
     {
-        return ReflectiveOperator(">=", left, right, () => left.Ge(right), () => right.Le(left));
+        return ReflectiveOperator(">=", left, right);
     }
 
     public static PyBoolObject Is(PyObject left, PyObject right)
