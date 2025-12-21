c920b197538b29343730fa6fe977cad1860d69b6
Silencersn <silencersn@outlook.com>
2025-12-21 19:58:01 +0800

    重构 PyRangeIteratorObject 类型及接口实现
    重命名并调整 PyRangeIteratorObjectType 的虚方法接口，统一采用 PyCallContext 和 self 参数，提升类型安全性。_enumerator 字段改为 internal 以便类型对象访问，StopIteration 和 TypeError 抛出方式改用 PyResult。引入 PySharp.PyRuntime.Calls 并精简类型对象定义，增强可维护性和扩展性。

--- PySharp/PyModules/Builtins/PyRangeIteratorObject.cs
+++ PySharp/PyModules/Builtins/PyRangeIteratorObject.cs
@@ -1,38 +1,37 @@
 ﻿using PySharp.PyRuntime;
+using PySharp.PyRuntime.Calls;
 
 namespace PySharp.PyModules.Builtins;
 
 public class PyRangeIteratorObject : PyObject
 {
-    private readonly IEnumerator<PyIntObject> _enumerator;
-
+    internal readonly IEnumerator<PyIntObject> _enumerator;
     public override PyTypeObject DefaultPyType => PyRangeIteratorObjectType.Shared;
 
     internal PyRangeIteratorObject(IEnumerable<PyIntObject> enumerable)
     {
         _enumerator = enumerable.GetEnumerator();
     }
-
-    protected internal override PyObject? IterImpl()
-    {
-        return this;
-    }
-
-    protected internal override PyObject? NextImpl()
-    {
-        if (!_enumerator.MoveNext())
-            return PyVirtualMachine.RaiseStopIteration();
-
-        return _enumerator.Current;
-    }
 }
 
-public sealed class PyRangeIteratorObjectType : PyPrimitiveTypeObject<PyRangeIteratorObjectType, PyRangeIteratorObject>
+public sealed class PyRangeIteratorObjectType : PyTypeObject<PyRangeIteratorObjectType, PyRangeIteratorObject>
 {
     public override string Name => "range_iterator";
 
-    protected internal override PyObject? NewImpl(PyTypeObject cls, IReadOnlyList<PyObject> args, IReadOnlyDictionary<string, PyObject> kwargs)
+    protected internal override PyResult Iter(PyCallContext context, PyRangeIteratorObject self)
     {
-        return PyVirtualMachine.RaiseTypeError(null);
+        return self;
+    }
+
+    protected internal override PyResult Next(PyCallContext context, PyRangeIteratorObject self)
+    {
+        if (!self._enumerator.MoveNext())
+            return PyResult.RaiseStopIteration();
+        return self._enumerator.Current;
+    }
+
+    protected internal override PyResult New(PyCallContext context, PyTypeObject cls, IReadOnlyList<PyObject> args, IReadOnlyDictionary<string, PyObject> kwargs)
+    {
+        return PyResult.RaiseTypeError(null);
     }
 }
