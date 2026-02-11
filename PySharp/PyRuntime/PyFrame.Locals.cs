d5dffc6b5e2fd0e4b183be82160e86ec73a8ec57
Silencersn <silencersn@outlook.com>
2026-02-11 22:08:01 +0800

    重构 PyFrame.Locals 及 LocalDictionary 实现
    重构了 PyFrame.Locals.cs 中 LocalDictionary 的结构与用法，移除对 PyFrameLocals 的依赖，改为直接持有 _localsTable 和 _localsPlus，并支持可选的 _extraLocals。调整了构造与克隆逻辑，优化了 Add、Remove、Contains、TryGetValue 等方法，提升健壮性和性能。接口实现方式更规范，代码更简洁易维护。引入 PyObjectComparer.Default 以保证 PyObject? 比较的一致性。整体提升了局部变量管理的灵活性和可读性。

--- PySharp/PyRuntime/PyFrame.Locals.cs
+++ PySharp/PyRuntime/PyFrame.Locals.cs
@@ -1,4 +1,5 @@
 ﻿using PySharp.PyModules.Builtins;
+using PySharp.PyRuntime.Comparison;
 using PySharp.Utility;
 using System.Collections;
 using System.Collections.Frozen;
@@ -39,7 +40,7 @@ partial class PyFrame
         }
 
         internal PyObject?[] LocalsPlus => _localsPlus;
-        public IDictionary<string, PyObject?> Locals => _locals ??= new LocalDictionary(this);
+        public IDictionary<string, PyObject?> Locals => _locals ??= new LocalDictionary(_localsTable, _localsPlus);
         public DictAdapter LocalsAdapter => _localsAdapter ??= new DictAdapter(Locals);
         public PyDictObject PyDict => _pyDict ??= PyDictObject.CreateProxy(LocalsAdapter);
 
@@ -49,7 +50,7 @@ partial class PyFrame
 
             if (_locals is LocalDictionary localDict)
             {
-                clone._locals = new LocalDictionary(clone, localDict._extraLocals.ToDictionary());
+                clone._locals = new LocalDictionary(_localsTable, clone._localsPlus, localDict.ExtraLocals?.ToDictionary());
                 if (_localsAdapter is not null)
                     clone._localsAdapter = new DictAdapter(clone._locals, _localsAdapter._extraDict.ToDictionary());
             }
@@ -62,143 +63,168 @@ partial class PyFrame
 
         private sealed class LocalDictionary : IDictionary<string, PyObject?>
         {
-            private readonly PyFrameLocals _locals;
-            private PyObject?[] LocalPlus => _locals._localsPlus;
-            private FrozenDictionary<string, int> LocalsTable => _locals._localsTable;
-            internal readonly Dictionary<string, PyObject?> _extraLocals;
+            private readonly FrozenDictionary<string, int> _localsTable;
+            private readonly PyObject?[] _localsPlus;
+            private Dictionary<string, PyObject?>? _extraLocals;
 
-            public LocalDictionary(PyFrameLocals locals) : this(locals, [])
+            internal Dictionary<string, PyObject?>? ExtraLocals => _extraLocals;
+
+            internal LocalDictionary(FrozenDictionary<string, int> localsTable, PyObject?[] localsPlus, Dictionary<string, PyObject?>? extraLocals)
             {
-            }
-            internal LocalDictionary(PyFrameLocals locals, Dictionary<string, PyObject?> extraLocals)
-            {
-                _locals = locals;
+                _localsPlus = localsPlus;
+                _localsTable = localsTable;
                 _extraLocals = extraLocals;
+            }
+            internal LocalDictionary(FrozenDictionary<string, int> localsTable, PyObject?[] localsPlus) : this(localsTable, localsPlus, null)
+            {
             }
 
             public PyObject? this[string key]
             {
                 get
                 {
-                    if (LocalsTable.TryGetValue(key, out var idx))
-                        return LocalPlus[idx];
-                    if (_extraLocals.TryGetValue(key, out var val))
-                        return val;
-                    return null;
+                    if (_localsTable.TryGetValue(key, out var index))
+                        return _localsPlus[index];
+
+                    return _extraLocals?.GetValueOrDefault(key);
                 }
                 set
                 {
-                    if (LocalsTable.TryGetValue(key, out var idx))
-                        LocalPlus[idx] = value;
-                    else
-                        _extraLocals[key] = value;
+                    if (_localsTable.TryGetValue(key, out var index))
+                    {
+                        _localsPlus[index] = value;
+                        return;
+                    }
+                    _extraLocals ??= [];
+                    _extraLocals[key] = value;
                 }
             }
 
-            public ICollection<string> Keys => [.. LocalsTable.Keys, .. _extraLocals.Keys];
+            public ICollection<string> Keys => _extraLocals is null ? _localsTable.Keys : [.. _localsTable.Keys, .. _extraLocals.Keys];
 
-            public ICollection<PyObject?> Values => [.. LocalsTable.Keys.Select(k => LocalPlus[LocalsTable[k]]), .. _extraLocals.Values];
+            public ICollection<PyObject?> Values => _extraLocals is null ? [.._localsPlus] : [.. _localsPlus, .. _extraLocals.Values];
 
-            public int Count => LocalsTable.Count + _extraLocals.Count;
+            public int Count => _localsTable.Count + _extraLocals?.Count ?? 0;
 
-            public bool IsReadOnly => false;
+            bool ICollection<KeyValuePair<string, PyObject?>>.IsReadOnly => false;
 
             public void Add(string key, PyObject? value)
             {
-                if (LocalsTable.TryGetValue(key, out var idx))
+                if (_localsTable.TryGetValue(key, out var index))
                 {
-                    if (LocalPlus[idx] is not null)
-                        throw new ArgumentException($"Key '{key}' already exists.");
-                    LocalPlus[idx] = value;
+                    if (_localsPlus[index] is not null)
+                        throw new ArgumentException($"An item with the same key has already been added. Key: {key}");
+                    _localsPlus[index] = value;
                 }
                 else
                 {
-                    if (_extraLocals.ContainsKey(key))
-                        throw new ArgumentException($"Key '{key}' already exists.");
+                    _extraLocals ??= [];
                     _extraLocals.Add(key, value);
                 }
             }
 
-            public void Add(KeyValuePair<string, PyObject?> item) => Add(item.Key, item.Value);
+            void ICollection<KeyValuePair<string, PyObject?>>.Add(KeyValuePair<string, PyObject?> item)
+            {
+                Add(item.Key, item.Value);
+            }
 
             public void Clear()
             {
-                for (int i = 0; i < LocalPlus.Length; i++)
-                    LocalPlus[i] = null;
-                _extraLocals.Clear();
+                _localsPlus.AsSpan().Clear();
+                _extraLocals?.Clear();
             }
 
-            public bool Contains(KeyValuePair<string, PyObject?> item)
+            bool ICollection<KeyValuePair<string, PyObject?>>.Contains(KeyValuePair<string, PyObject?> item)
             {
-                if (LocalsTable.TryGetValue(item.Key, out var idx))
-                    return Equals(LocalPlus[idx], item.Value);
-                return _extraLocals.TryGetValue(item.Key, out var val) && Equals(val, item.Value);
+                if (_localsTable.TryGetValue(item.Key, out var index))
+                    return PyObjectComparer.Default.Equals(_localsPlus[index], item.Value);
+
+                if (_extraLocals is null)
+                    return false;
+
+                return _extraLocals.TryGetValue(item.Key, out var value) && PyObjectComparer.Default.Equals(value, item.Value);
             }
 
             public bool ContainsKey(string key)
             {
-                if (LocalsTable.ContainsKey(key))
+                if (_localsTable.ContainsKey(key))
                     return true;
-                return _extraLocals.ContainsKey(key);
+
+                return _extraLocals?.ContainsKey(key) ?? false;
             }
 
-            public void CopyTo(KeyValuePair<string, PyObject?>[] array, int arrayIndex)
+            void ICollection<KeyValuePair<string, PyObject?>>.CopyTo(KeyValuePair<string, PyObject?>[] array, int arrayIndex)
             {
-                foreach (var k in LocalsTable.Keys)
-                    array[arrayIndex++] = new KeyValuePair<string, PyObject?>(k, LocalPlus[LocalsTable[k]]);
-                foreach (var kv in _extraLocals)
-                    array[arrayIndex++] = kv;
+                ArgumentOutOfRangeException.ThrowIfGreaterThan(arrayIndex + Count, array.Length);
+
+                foreach (var pair in _localsTable)
+                    array[arrayIndex++] = KeyValuePair.Create(pair.Key, _localsPlus[pair.Value]);
+
+                if (_extraLocals is null)
+                    return;
+
+                foreach (var pair in _extraLocals)
+                    array[arrayIndex++] = pair;
             }
 
             public IEnumerator<KeyValuePair<string, PyObject?>> GetEnumerator()
             {
-                foreach (var k in LocalsTable.Keys)
-                    yield return new KeyValuePair<string, PyObject?>(k, LocalPlus[LocalsTable[k]]);
-                foreach (var kv in _extraLocals)
-                    yield return kv;
+                foreach (var pair in _localsTable)
+                    yield return KeyValuePair.Create(pair.Key, _localsPlus[pair.Value]);
+
+                if (_extraLocals is null)
+                    yield break;
+
+                foreach (var pair in _extraLocals)
+                    yield return pair;
             }
 
             public bool Remove(string key)
             {
-                if (LocalsTable.TryGetValue(key, out var idx))
+                if (_localsTable.TryGetValue(key, out var index))
                 {
-                    if (LocalPlus[idx] is not null)
-                    {
-                        LocalPlus[idx] = null;
-                        return true;
-                    }
-                    return false;
+                    if (_localsPlus[index] is null)
+                        return false;
+
+                    _localsPlus[index] = null;
+                    return true;
                 }
-                return _extraLocals.Remove(key);
+
+                return _extraLocals?.Remove(key) ?? false;
             }
 
-            public bool Remove(KeyValuePair<string, PyObject?> item)
+            bool ICollection<KeyValuePair<string, PyObject?>>.Remove(KeyValuePair<string, PyObject?> item)
             {
-                if (LocalsTable.TryGetValue(item.Key, out var idx))
+                if (_localsTable.TryGetValue(item.Key, out var index))
                 {
-                    if (Equals(LocalPlus[idx], item.Value))
-                    {
-                        LocalPlus[idx] = null;
-                        return true;
-                    }
-                    return false;
+                    if (_localsPlus[index] is null)
+                        return item.Value is null;
+
+                    if (!PyObjectComparer.Default.Equals(_localsPlus[index], item.Value))
+                        return false;
+
+                    _localsPlus[index] = null;
+                    return true;
                 }
-                if (_extraLocals.TryGetValue(item.Key, out var val) && Equals(val, item.Value))
-                    return _extraLocals.Remove(item.Key);
-                return false;
+
+                return (_extraLocals as ICollection<KeyValuePair<string, PyObject?>>)?.Remove(item) ?? false;
             }
 
             public bool TryGetValue(string key, [MaybeNullWhen(false)] out PyObject? value)
             {
-                if (LocalsTable.TryGetValue(key, out var idx))
+                if (_localsTable.TryGetValue(key, out var index))
                 {
-                    value = LocalPlus[idx];
+                    value = _localsPlus[index];
                     return true;
                 }
-                if (_extraLocals.TryGetValue(key, out value))
-                    return true;
-                value = null;
-                return false;
+
+                if (_extraLocals is null)
+                {
+                    value = null;
+                    return false;
+                }
+
+                return _extraLocals.TryGetValue(key, out value);
             }
 
             IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
