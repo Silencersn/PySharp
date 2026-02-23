7cc172dc2f45cc29d6c8419fe675761dc8096032
Silencersn <silencersn@outlook.com>
2026-02-23 10:07:29 +0800

    引入 PySharp.SourceGeneration 源生成器项目
    新增 PySharp.SourceGeneration 项目并集成至解决方案，主项目以 Analyzer 方式引用。添加了空的 SlotsGenerator 增量源生成器类，并配置了相关依赖和编译选项，为后续自动代码生成功能打下基础。

---
+++ PySharp.SourceGeneration/SlotsGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PySharp.SourceGeneration;
+
+[Generator]
+public class SlotsGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+    }
+}
+
