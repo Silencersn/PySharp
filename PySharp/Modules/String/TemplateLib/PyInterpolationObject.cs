7e5bf13891f5d433baaf50bc78347d92e97c9111
Silencersn <silencersn@outlook.com>
2026-03-22 19:16:39 +0800

    新增字符串模板与插值对象类型支持
    新增 PyInterpolationObject 和 PyTemplateObject 两个类，分别用于表示字符串插值和模板对象，并通过 PyType 属性注册为 Python 类型，暴露相关只读属性，便于在 PySharp 的 Python 运行时中进行字符串模板和插值操作。

---
+++ PySharp/Modules/String/TemplateLib/PyInterpolationObject.cs
@@ -0,0 +1,49 @@
+﻿using PySharp.Modules.Builtins;
+using PySharp.Runtime.Calls;
+using PySharp.Runtime.PyAttributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PySharp.Modules.String.TemplateLib;
+
+public sealed class PyInterpolationObject : PyObject
+{
+    internal readonly PyObject _value;
+    internal readonly PyStrObject _expression;
+    internal readonly PyStrObject _conversion;
+    internal readonly PyStrObject _formatSpec;
+
+    public PyInterpolationObject(PyObject value, PyStrObject expression, PyStrObject conversion, PyStrObject formatSpec)
+    {
+        _value = value;
+        _expression = expression;
+        _conversion = conversion;
+        _formatSpec = formatSpec;
+    }
+}
+
+[PyType("Interpolation", Module = "string.templatelib")]
+public sealed partial class PyInterpolationObjectType : PyTypeObject<PyInterpolationObjectType, PyInterpolationObject>
+{
+    [PyProperty("value")]
+    private static PyResult Get_Value(PyCallContext context, PyInterpolationObject self)
+    {
+        return self._value;
+    }
+    [PyProperty("expression")]
+    private static PyResult Get_Expression(PyCallContext context, PyInterpolationObject self)
+    {
+        return self._expression;
+    }
+    [PyProperty("conversion")]
+    private static PyResult Get_Conversion(PyCallContext context, PyInterpolationObject self)
+    {
+        return self._conversion;
+    }
+    [PyProperty("format_spec")]
+    private static PyResult Get_FormatSpec(PyCallContext context, PyInterpolationObject self)
+    {
+        return self._formatSpec;
+    }
+}
---
+++ PySharp/Modules/String/TemplateLib/PyTemplateObject.cs
@@ -0,0 +1,36 @@
+﻿using PySharp.Modules.Builtins;
+using PySharp.Runtime;
+using PySharp.Runtime.Calls;
+using PySharp.Runtime.PyAttributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PySharp.Modules.String.TemplateLib;
+
+public sealed class PyTemplateObject : PyObject
+{
+    internal readonly PyTupleObject _strings;
+    internal readonly PyTupleObject _interpolations;
+
+    internal PyTemplateObject(PyTupleObject strings, PyTupleObject interpolations)
+    {
+        _strings = strings;
+        _interpolations = interpolations;
+    }
+}
+
+[PyType("Template", Module = "string.templatelib")]
+public sealed partial class PyTemplateObjectType : PyTypeObject<PyTemplateObjectType, PyTemplateObject>
+{
+    [PyProperty("strings")]
+    private static PyResult Get_Strings(PyCallContext context, PyTemplateObject self)
+    {
+        return self._strings;
+    }
+    [PyProperty("interpolations")]
+    private static PyResult Get_Interpolations(PyCallContext context, PyTemplateObject self)
+    {
+        return self._interpolations;
+    }
+}
