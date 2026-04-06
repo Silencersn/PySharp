c3a637e5a691097f5a71f68ad9a1d70dd9214c77
Silencersn <silencersn@outlook.com>
2026-04-06 09:15:15 +0800

    新增 TypeAliasType 类型支持于 typing 模块
    实现了 PyTypeAliasTypeObject 及其类型对象，支持 typing.TypeAliasType 的基本结构和 __name__ 属性访问，为 PySharp 的 typing 模块扩展类型别名功能奠定基础。

---
+++ PySharp/Modules/Typing/PyTypeAliasTypeObject.cs
@@ -0,0 +1,32 @@
+﻿using PySharp.Modules.Builtins;
+using PySharp.Runtime;
+using PySharp.Runtime.Calls;
+using PySharp.Runtime.PyAttributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PySharp.Modules.Typing;
+
+internal sealed class PyTypeAliasTypeObject : PyObject
+{
+    public override PyTypeObject DefaultPyType => PyTypeAliasTypeObjectType.Shared;
+
+    internal readonly string _name;
+
+    internal PyTypeAliasTypeObject(string name)
+    {
+        _name = name;
+    }
+}
+
+
+[PyType("TypeAliasType", Module = "typing")]
+internal sealed partial class PyTypeAliasTypeObjectType : PyTypeObject<PyTypeAliasTypeObject>
+{
+    [PyProperty(PySpecialNames.Name)]
+    private static PyResult Get_Name(PyCallContext context, PyTypeAliasTypeObject self)
+    {
+        return PyStrObject.FromString(self._name);
+    }
+}
